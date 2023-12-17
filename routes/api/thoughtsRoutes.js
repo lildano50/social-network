a1595a8a13424146f0b1091541d0c0d4c2875b8c
Daniel Park <danielapark87@gmail.com>
2023-12-16 19:37:16 -0600

    reaction routes added

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -5,8 +5,10 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  deleteReaction,
+  addReaction
   } = require('../../controllers/thoughtsController.js');
-const { getSingleUser } = require('../../controllers/userController.js');
+// const { getSingleUser } = require('../../controllers/userController.js');
 
 //Get all thoughts
 router.route('/').get(getThoughts);
@@ -14,14 +16,22 @@ router.route('/').get(getThoughts);
 //Get a single thought by ID
 router.route('/:thoughtId').get(getSingleThought);
 
-//Create a thought and push associated thought to user's thoughts array field
-router.route('/:userId/thoughts').get(getSingleUser).post(createThought);
+// //Create a thought and push associated thought to user's thoughts array field
+// router.route('/:userId/thoughts').get(getSingleUser).post(createThought);
 
 //Update a single thought by ID
 router.route('/:thoughtId/update').get(getSingleThought).put(updateThought);
 
 //Delete a single thought by ID
 router.route('/:thoughtId').delete(deleteThought);
+
+// ***Reactions***
+
+// Add reaction to thought
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// Delete reaction to thought
+router.route('/:thoughtId/reactions/').delete(deleteReaction);
 
 
 
